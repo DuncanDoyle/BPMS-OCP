c0d10769c3ab7dd47cd8750307727aba3aa92f94
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-04-13 17:50:23 +0200

    Added logging and fixed variable names.

--- ocp-mortgage-dashboard/src/main/webapp/js/controllers/home.js
+++ ocp-mortgage-dashboard/src/main/webapp/js/controllers/home.js
@@ -3,20 +3,23 @@
 angular.module('app')
     .controller("HomeCtrl", ['$scope', '$http', 'Constants', function ($scope, $http, Constants) {
 
-        $scope.processes = {};
         $scope.consts = Constants;
 
         $http.get("service/home/processes").success(function (data) {
-            console.log("Retrieved data.");
+            console.log("Retrieved process data.");
             console.log(data);
-            $scope.processes.result = data;
+            $scope.processes = data;
         });
 
         $http.get("service/home/running").success(function (data) {
+            console.log("Retrieved instance data.");
+            console.log(data);
             $scope.running = data;
         });
 
         $http.get("service/home/workitems").success(function (data) {
+            console.log("Retrieved workitems.");
+            console.log(data);
             $scope.workItems = data;
         });
     }]);
