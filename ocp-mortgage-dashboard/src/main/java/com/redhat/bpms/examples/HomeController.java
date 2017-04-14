569592adeb81d5081e990814d6def2a8154461ec
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-04-14 08:46:54 +0200

    Hardcoded some stuff to test serialization.

--- ocp-mortgage-dashboard/src/main/java/com/redhat/bpms/examples/HomeController.java
+++ ocp-mortgage-dashboard/src/main/java/com/redhat/bpms/examples/HomeController.java
@@ -1,9 +1,8 @@
 package com.redhat.bpms.examples;
 
-import com.redhat.bpms.examples.service.MgmtClientService;
-import org.kie.server.api.model.definition.ProcessDefinition;
-import org.kie.server.api.model.instance.ProcessInstance;
-import org.kie.server.api.model.instance.WorkItemInstance;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -11,7 +10,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
+
+import org.kie.server.api.model.definition.ProcessDefinition;
+import org.kie.server.api.model.instance.ProcessInstance;
+import org.kie.server.api.model.instance.WorkItemInstance;
+
+import com.redhat.bpms.examples.service.MgmtClientService;
 
 /***
  * @author jary
@@ -28,16 +32,42 @@ public class HomeController {
     @Path("/processes")
     @Produces(MediaType.APPLICATION_JSON)
     public List<ProcessDefinition> listProcesses() {
-
-        return mgmtClientService.listProcesses();
+    	List<ProcessDefinition> returnList = new ArrayList<>();
+    	ProcessDefinition definition = new ProcessDefinition();
+    	
+    	definition.setContainerId("containerId");
+    	definition.setId("id");
+    	definition.setName("Name");
+    	definition.setPackageName("PackageName");
+    	definition.setVersion("1.42");
+    	returnList.add(definition);
+    	return returnList;
+    	
+    	
+    	
+        //return mgmtClientService.listProcesses();
     }
 
     @GET
     @Path("/running")
     @Produces(MediaType.APPLICATION_JSON)
     public List<ProcessInstance> listProcessInstances() {
-
-        return mgmtClientService.listProcessInstances();
+    	
+    	List<ProcessInstance> returnList = new ArrayList<>();
+    	ProcessInstance instance = new ProcessInstance();
+    	instance.setContainerId("bla");
+    	instance.setDate(new Date());
+    	instance.setId(42L);
+    	instance.setProcessInstanceDescription("Description");
+    	instance.setProcessName("ProcessName");
+    	instance.setProcessVersion("1.42");
+    	instance.setProcessId("ProcessId");
+    	
+    	returnList.add(instance);
+    	
+    	return returnList;
+    	
+    	//return mgmtClientService.listProcessInstances();
     }
 
     @GET
